5dd1ebab1abbcfb959bb9fc0df1ed042fd71bdb2
haileyhansard <haileyhansard@gmail.com>
2020-08-19 19:45:09 -0700

    Tests passing for success, fail, repair

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,25 @@ module.exports = {
   success,
   fail,
   repair,
-  get,
+  //get,
 };
 
 function success(item) {
+  if(item.enhancement < 20) item.enhancement += 1;
   return { ...item };
 }
 
 function fail(item) {
+  if(item.enhancement < 15) item.durability -= 5;
+  if(item.enhancement > 14) item.durability -= 10;
+  if(item.enhancement > 15) item.enhancement -= 1;
   return { ...item };
 }
+//cannot use equal to, the test wont work
 
 function repair(item) {
-  return { ...item };
+  if(item.durability < 100) item.durability = 100;
+  return { ...item};
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,55 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require('../enhancing/enhancer');
 // test away!
+
+// repair
+// success
+// fail
+
+describe('Check if repair function works', () => {
+    it('should restore durability of item to 100', () => {
+        const item = {  name: 'mario', durability: 70, enhancement: 20 }
+        const newItem = enhancer.repair(item);
+        expect(newItem.durability).toBe(100);
+    })
+    it('should not increase durability when at 100', () => {
+        const item = { name: 'mario', durability: 100, enhancement: 0 }
+        const newItem = enhancer.repair(item);
+        expect(newItem.durability).toBe(100);
+    })
+});
+
+describe('Check if success function works', () => {
+    it('should increase enhancement by 1', () => {
+        const item = { name: 'peach', durability: 85, enhancement: 5}
+        const newItem = enhancer.success(item);
+        expect(newItem.enhancement).toBe(6);
+    })
+    it('should not increase or change enhancement if enhancement is at 20', () => {
+        const item = { name: 'peach', durability: 80, enhancement: 20}
+        const newItem = enhancer.success(item);
+        expect(newItem.enhancement).toBe(20);
+    })
+});
+
+describe('Check if fail function works', () => {
+    it('should decrease durability by 5 if enhancement is less than 15', () => {
+        const item = { name: 'luigi', durability: 90, enhancement: 14}
+        const newItem = enhancer.fail(item);
+        expect(newItem.durability).toBe(85);
+    })
+    it('should decrease durability by 10 if enhancement is at 15', () => {
+        const item = { name: 'luigi', durability: 85, enhancement: 15}
+        const newItem = enhancer.fail(item);
+        expect(newItem.durability).toBe(75);
+    })
+    it('should decrease durability by 10 if enhancement is at 16', () => {
+        const item = { name: 'luigi', durability: 75, enhancement: 16}
+        const newItem = enhancer.fail(item);
+        expect(newItem.durability).toBe(65);
+    })
+    it('should decrease enhancement level by 1 if the current enhancement level is greater than 16', () => {
+        const item = { name: 'luigi', durability: 85, enhancement: 17}
+        const newItem = enhancer.fail(item);
+        expect(newItem.enhancement).toBe(16);
+    })
+})
